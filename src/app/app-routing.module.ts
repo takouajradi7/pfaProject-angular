f150e4f16182deb58790ab5e21c723a092dc50ea
takouajradi7 <takoua.jradi@enis.tn>
2023-05-02 19:39:42 +0100

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,14 @@ import { UsersComponent } from './users/users.component';
 import { AddUsersComponent } from './add-users/add-users.component';
 import { UpdateUsersComponent } from './update-users/update-users.component';
 
+
+
 const routes: Routes = [
   {path: "users", component : UsersComponent},
   {path: "add-users", component : AddUsersComponent},
-  {path: "", redirectTo: "users", pathMatch: "full" },
+  {path: "", redirectTo: "app", pathMatch: "full" },
   {path: "updateUser/:id", component: UpdateUsersComponent},
+  
 
 
 ];
